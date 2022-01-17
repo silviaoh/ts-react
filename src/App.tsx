535a37fa7020417fac1719628d2a735ccfa6cfca
silviaoh <silvia.jisu.oh@gmail.com>
2022-01-18 01:02:47 +0900

    ts example

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Greetings from './Greetings';
-import Counter from './Counter';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Greetings from "./Greetings";
+import Counter from "./Counter";
+import MyForm from "./MyForm";
+
+interface submitTypes {
+  name: string;
+  description: string;
+}
 
 function App() {
-	const onClick = (name: string) => {
-		console.log(`say hello ${name}`);
-		return true;
-	};
-	return (
-		<div className="App">
-			<Greetings name="jisu" mark="yellow" optional onClick={onClick} />
-			<Counter />
-		</div>
-	);
+  const onClick = (name: string) => {
+    console.log(`say hello ${name}`);
+    return true;
+  };
+
+  const onSubmit = (form: submitTypes) => {
+    console.log(`form`, form);
+  };
+  return (
+    <div className="App">
+      <Greetings name="jisu" mark="yellow" optional onClick={onClick} />
+      <Counter />
+      <MyForm onSubmit={onSubmit} />
+    </div>
+  );
 }
 
 export default App;
